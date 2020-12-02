134d941abb24d61fa84fcdb1183444368e51e479
amrowka234939 <amrowka234939@e-science.pl>
2020-12-02 16:08:06 +0100

    Added second solution

---
+++ 02/ex02.py
@@ -0,0 +1,46 @@
+def parse(line):
+    s = line.split(" ")
+    a, b = s[0].split("-")
+    letter = s[1][0]
+    password = s[2][:-1]
+
+    return a, b, letter, password
+
+
+def validate1(let_min, let_max, letter, password):
+    counter = password.count(letter)
+    if int(let_min) <= counter <= int(let_max):
+        return True
+    return False
+
+
+def validate2(a, b, letter, password):
+    if password[int(a)-1] == letter and password[int(b)-1] != letter:
+        return True
+    if password[int(a) - 1] != letter and password[int(b) - 1] == letter:
+        return True
+    return False
+
+
+# Part 1
+
+answer = 0
+lines = open('input.txt', 'r').readlines()
+
+for line in lines:
+    if validate1(*parse(line)):
+        answer += 1
+
+print ("Answer1: ", answer)
+
+# Part 2
+
+answer = 0
+
+for line in lines:
+    if validate2(*parse(line)):
+        answer += 1
+
+print ("Answer2: ", answer)
+
+
